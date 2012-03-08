a66cdeed5784233b854f44a499388ff1f770a61c
Facundo Viale <facundo.m.viale@gmail.com>
2012-03-08 14:53:36 -0300

    Remote list dir

--- src/ext2_server/ext2_server.c
+++ src/ext2_server/ext2_server.c
@@ -6,7 +6,18 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
+#include <google/protobuf-c/protobuf-c-rpc.h>
+
+#include "../commons/rpc_layer.pb-c.h"
+
 #include "ext2_server.h"
+
+
+static t_ext2_server *ext2_server;
+
+// -----------  RPC Functions  -----------------
+static void ext2_service__read_dir(RpcLayer__Ext2Services_Service *, const RpcLayer__ReadDirRequest *, RpcLayer__ReadDirResponse_Closure, void *);
+static RpcLayer__Ext2Services_Service ext2_service = RPC_LAYER__EXT2_SERVICES__INIT(ext2_service__);
 
 
 t_ext2_server *ext2_server_create(char* device_path){
@@ -45,18 +56,48 @@ t_ext2_server *ext2_server_create(char* device_path){
 
 void ext2_server_run(t_ext2_server *self){
 
+	const char name[] = "socket";
 
-	t_list *elements = ext2_list_dir(self->fs, "/");
+	protobuf_c_rpc_server_new(PROTOBUF_C_RPC_ADDRESS_LOCAL, name, (ProtobufCService *) &ext2_service, NULL);
 
-	if( elements != NULL ){
-
-		void _print(char *entry){
-			printf("%s\n", entry);
-		}
-
-		list_iterate(elements, _print);
-	}
-
-	list_destroy_and_destroy_elements(elements, free);
+	for (;;)
+		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
 
 }
+
+static void ext2_service__read_dir(RpcLayer__Ext2Services_Service *service,
+									const RpcLayer__ReadDirRequest *request,
+									RpcLayer__ReadDirResponse_Closure closure,
+									void *closure_data) {
+
+	RpcLayer__ReadDirResponse response = RPC_LAYER__READ_DIR_RESPONSE__INIT;
+
+	t_list *elements = ext2_list_dir(ext2_server->fs, "/");
+
+	if (elements != NULL && list_size(elements) > 0) {
+
+		response.n_elements = list_size(elements);
+		response.elements = calloc(list_size(elements), sizeof(char*));
+
+		int index;
+		for(index = 0; index < list_size(elements); index ++){
+			response.elements[index] = list_get(elements, index);
+		}
+
+	} else {
+		response.n_elements = 0;
+	}
+
+	closure(&response, closure_data);
+
+	list_destroy_and_destroy_elements(elements, free);
+}
+
+int main(int argc, char **argv) {
+
+	ext2_server = ext2_server_create("dev/ext2.disk");
+	ext2_server_run(ext2_server);
+
+	return EXIT_FAILURE;
+}
+
---
+++ src/fuse_client/fuse_client.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <sys/stat.h>
+#include <google/protobuf-c/protobuf-c-rpc.h>
+
+#include "../commons/rpc_layer.pb-c.h"
+
+ProtobufCService *service;
+
+static int fuse_ext2_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+	RpcLayer__ReadDirRequest query = RPC_LAYER__READ_DIR_REQUEST__INIT;
+	protobuf_c_boolean is_done = 0;
+
+	query.path = strdup(path);
+
+	void _handle_response(const RpcLayer__ReadDirResponse *result, void *closure_data) {
+
+		int index;
+		for (index = 0; index < result->n_elements; index++) {
+			filler(buf, result->elements[index], NULL, 0);
+		}
+
+		*(protobuf_c_boolean *) closure_data = 1;
+	}
+
+	rpc_layer__remote_ext2__read_dir(service, &query, _handle_response, &is_done);
+
+	while (!is_done)
+		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
+
+	free(query.path);
+
+	return 0;
+}
+
+static struct fuse_operations ext2_operations = {
+        //.getattr = NULL,
+        .readdir = fuse_ext2_readdir
+};
+
+int main(int argc, char **argv) {
+	ProtobufC_RPC_Client *client;
+	const char *name = "socket";
+
+	service = protobuf_c_rpc_client_new(PROTOBUF_C_RPC_ADDRESS_LOCAL, name, &rpc_layer__remote_ext2__descriptor, NULL);
+
+	client = (ProtobufC_RPC_Client *) service;
+
+	printf("Connecting... ");
+	while (!protobuf_c_rpc_client_is_connected(client)){
+		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
+	}
+	printf("done.\n");
+
+
+	int ret;
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+
+	ret = fuse_main(args.argc, args.argv, &ext2_operations);
+
+	fuse_opt_free_args(&args);
+
+	return ret;
+}
+
